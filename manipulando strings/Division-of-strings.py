4b09aeaf7e3926ddfc4e428319a9590f37dd19ee
Bernardo AS <bernardosumbane160@gmail.com>
2025-08-23 19:33:49 -0700

    actualizei o novo repositorio

---
+++ manipulando strings/Division-of-strings.py
@@ -0,0 +1,9 @@
+nomes='Bernardo Arnaldo Sumbane'
+partes=nomes.split()
+juncao='-'.join(nomes)
+
+
+print(partes)
+print(juncao)
+
+""" o split - usamos para separar a variavel nomes em partes """
---
+++ manipulando strings/transformation-of-text.py
@@ -0,0 +1,13 @@
+nome='bene-SUMBANE'
+
+print(nome.replace('SUMBANE','ARNALDO'))
+print(nome.upper())
+print(nome.lower())
+print(nome.capitalize())
+print(nome.title())
+
+""" o  nome.replace('SUMBANE','ARNALDO')- usamos para substituir uma palavra noutra """
+""" o nome.upper()- usamos para colocar tudo em maiusculas """
+""" o nome.lower()- usamos para colocar tudo em menusculas """
+""" o nome.capitalize()- usamos para colocar a primeira letra em maiuscula e o resto em minusculas """
+""" o nome.title()- usamos para deixar todas as primeiras letras inicias em maiusculas """

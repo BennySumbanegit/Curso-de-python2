8e4460a385e3e6d41da85d97df4aa45ef6a76daf
Bernardo AS <bernardosumbane160@gmail.com>
2025-08-26 16:49:57 -0700

    actualizei

--- Exercices pyton/ex022.py
+++ Exercices pyton/ex022.py
@@ -2,11 +2,11 @@ nome = str(input('Digite seu nome completo:'))
 
 semespacos = nome.replace(' ','')
 
-dividir = nome.split(nome[0:5])
+dividir = nome.split()
 
 print('ola',nome,'!')
-print('o seu nome tem ',len(nome) ,'çaracteres')
+print('o seu nome tem ',len(nome) ,'caracteres')
 print('O seu nome em maiusculas fica',nome.upper())
 print('o seu nome em minusculas fica',nome.lower())
 print('o seu nome sem espacos fica com',len(semespacos) ,'caracteres')
-print('o primeiro bloco do seu nome dividido', dividir)
+print('o seu primeiro nome dividido fica', dividir[0])
